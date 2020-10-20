b84018854b4a617777013b62161caa52befa10b1
Omas Ajiri <jicintech@gmail.com>
2020-10-20 17:01:29 +0100

    feat: added dependencies

---
+++ app/src/main/java/com/gadscommunityproject/hype/HypeLandingActivity.kt
@@ -0,0 +1,11 @@
+package com.gadscommunityproject.hype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HypeLandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hype_landing)
+    }
+}
